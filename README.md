fad874b9b24a08d237ae181aa1027297750f1283
Oscar Barillas <oscarbarillas93@gmail.com>
2016-08-28 14:06:51 -0700

    Redux Book Example

---
+++ README.md
@@ -0,0 +1,8 @@
+# Redux Book Example
+
+Select a book and it will display the details on the right.
+
+## Directions
+npm install
+
+npm start
---
+++ src/actions/index.js
@@ -0,0 +1,12 @@
+export function selectBook(book) {
+  // selectbook is an action creator, it needs to return an action,
+  // an object with a type property
+
+  return {
+    type: 'BOOK_SELECTED',
+    payload: book
+    
+  }
+
+
+}
